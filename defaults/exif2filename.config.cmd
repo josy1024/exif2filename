b0eb4bcbf3186cb44115ad7591c32189d4975c63
josy1024 <josy1024@f5430724-b04b-0410-974b-555e431c3ee8>
2010-12-12 11:26:24 +0000

    * change config behavior to separate config file (rollout) * add parameter for start_exif2filename.cmd * add start_exif2filename.cmd as "default" start program * some fewer debug output in exif2filename.vbs

---
+++ defaults/exif2filename.config.cmd
@@ -0,0 +1,24 @@
+REM copy this file to same location of the vbs script and change the values you need:
+
+REM examples - change to your directies
+
+REM set basedir="E:\ABLAGE\%date:~6,4%\" 
+set basedir=%userprofile%\Pictures
+set basedir1=
+set basedir2=
+REM set basedir1=%userprofile%\Pictures
+REM set basedir2=%userprofile%\Pictures
+
+REM flat structure
+REM set makefolders=
+set makefolders=1 
+set makefolders1=1
+set makefolders2=1
+
+REM no prompt:
+REM set promptme=
+set promptme=1
+
+set changefiletime=
+REM set changefiletime1=
+REM set changefiletime2=
